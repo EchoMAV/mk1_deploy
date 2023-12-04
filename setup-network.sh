94eff6d0ef73b744acf3121a18d4476a8a5af086
B Stinson <bstinson@echomav.com>
2023-12-04 10:45:06 -0500

    wip

--- setup-network.sh
+++ setup-network.sh
@@ -84,9 +84,9 @@ elif [ $IP_INPUT = "dhcp" ]; then
   #if static-iface exists, then mod to dhcp
   exist=$(nmcli c show "static-$IFACE" 2>/dev/null)  
   if [ ! -z "$exist" ] ; then     # delete the interface if it exists
+        $SUDO nmcli con mod "static-$IFACE" ipv4.method auto
         $SUDO nmcli con mod "static-$IFACE" ipv4.gateway ""
         $SUDO nmcli con mod "static-$IFACE" ipv4.addresses ""
-        $SUDO nmcli con mod "static-$IFACE" ipv4.method auto
         $SUDO nmcli con down "static-$IFACE"
         $SUDO nmcli con up "static-$IFACE"
   else
