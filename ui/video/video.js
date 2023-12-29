dfbe3c4b61e5f75e420cd2117204eb903232d031
BradS <bstinson@echomav.com>
2023-12-29 14:18:54 -0500

    wip

--- ui/video/video.js
+++ ui/video/video.js
@@ -28,9 +28,13 @@ document.onload = InitPage();
 document.getElementById("save").addEventListener("click", SaveSettings);
 
 
+
+
 function InitPage() {
 
-
+        
+    var qrcode = new QRCode(document.getElementById("qrcode"), "http://jindo.dev.naver.com/collie");
+    
     cockpit.file(confLocation + "video.conf").read().then((content, tag) => SuccessReadFile(content))
     .catch(error => FailureReadFile(error));
 
@@ -102,7 +106,7 @@ function SuccessReadFile(content) {
            // videoPort.value = myConfig.VIDEOSERVER_PORT;
             videoName.value = myConfig.VIDEOSERVER_STREAMNAME;
             serverURL.innerHTML = "<a href='https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value + "' target='_blank'>https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value + "</a>";
-            qrcode.makeCode("https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value);
+            //qrcode.makeCode("https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value);
             AddDropDown(videoBitrate, serverBitrateArray, myConfig.VIDEOSERVER_BITRATE);
         }
         else{
@@ -190,7 +194,7 @@ function SaveSettings() {
     serverURL.innerHTML = "<a href='https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value + "' target='_blank'>https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value + "</a>";
 
     //generate the QR Code
-    qrcode.makeCode("https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value);
+    //qrcode.makeCode("https://" + videoHost.value + "/LiveApp/play.html?id=" + videoName.value);
     
     //start the pipeline back (unless disabled)
     if (bitRate!==0)
